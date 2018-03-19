e0ca88b70d89d3c6daf2c782572f9e9ae73cd2bd
Diederik <dijoluc@hotmail.com>
2018-03-19 23:06:28 +0100

    Opmaakfoutjes eruit gehaald.

--- EastOfLoathing/src/TEST.py
+++ EastOfLoathing/src/TEST.py
@@ -1,29 +1,32 @@
 #Character creation:
 
+import random
+import time
 
 protagonist_name = input('What is your name?')
 print('My name is', protagonist_name)
 
-import time
+
 time.sleep(1)
 print('Nice to meet you', protagonist_name,'!')
 time.sleep(1)
 #Class creation. 'Are you sure?'loop terug naar hier.
-protagonist_class = input('What class are you? You can choose warrior, priest, mage or thief!)')
+protagonist_class = input('What class are you? You can choose warrior, priest, mage or thief!')
 
-import random
-def class_strenght(str):
+power  = 0;
+
+def class_strenght(power):
     if protagonist_class == 'warrior' or protagonist_class == 'Warrior':
-        str = random.randint(10, 18)
-            print('Your strenght is', str)
+        power = random.randint(10, 18)
+        print('Your strenght is', power)
     elif protagonist_class == 'priest' or protagonist_class == 'Priest':
-        str = random.randint(8, 16)
-            print('Your strenght is', str)
+        power = random.randint(8, 16)
+        print('Your strenght is', power)
     elif protagonist_class == 'thief' or protagonist_class == 'Thief':
-        str = random.randint(6, 14)
-            print('Your strenght is', str)
+        power = random.randint(6, 14)
+        print('Your strenght is', power)
     elif protagonist_class == 'mage' or protagonist_class == 'Mage':
-        str = random.randint(4, 12)
-            print('Your strenght is', str)
+        power = random.randint(4, 12)
+        print('Your strenght is', power)
     else:
-        rint('Error')
+        print('Error')
