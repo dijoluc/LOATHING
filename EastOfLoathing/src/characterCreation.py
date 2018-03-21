2a75d53a6670f78a13d26293fb29b5f705df0e60
Diederik <dijoluc@hotmail.com>
2018-03-21 14:20:04 +0100

    Updates Prot dient defined te worden

--- EastOfLoathing/src/characterCreation.py
+++ EastOfLoathing/src/characterCreation.py
@@ -2,7 +2,6 @@
 
 import random
 import time
-from django.contrib.gis.db.models.lookups import OverlapsLeftLookup #djano is een spciek framework...waarom?
 
 class CharacterCreation(object):
     strength  = 0;     
@@ -33,68 +32,68 @@ class CharacterCreation(object):
         print('are you sure you want to be a', protagonist_class)
         flag = input('Yes or No?').lower()
 
-        def class_strenght(strength):
-            if prot == 'warrior': 
-                strength = random.randint(10, 18)
-                print('Your strenght is', strength)
-            elif prot == 'priest':
-                strength = random.randint(8, 16)
-                print('Your strenght is', strength)
-            elif prot == 'thief': 
-                strength = random.randint(6, 14)
-                print('Your strenght is', strength)
-            else:
+    def class_strenght(self):
+        if prot == 'warrior': 
+            strength = random.randint(10, 18)
+            print('Your strenght is', strength)
+        elif prot == 'priest':
+            strength = random.randint(8, 16)
+            print('Your strenght is', strength)
+        elif prot == 'thief': 
+            strength = random.randint(6, 14)
+            print('Your strenght is', strength)
+        else:
                 strength = random.randint(4, 12)
                 print('Your strenght is', strength)
         
-        def class_constitution(constitution):
-            if prot == 'warrior': 
-                constitution = random.randint(10, 18)
-                print('Your constitution is', constitution)
-            elif prot == 'priest':
-                constitution = random.randint(10, 18)
-                print('Your constitution is', constitution)
-            elif prot == 'thief': 
-                constitution = random.randint(8, 14)
-                print('Your constitution is', constitution)
-            else:
-                constitution = random.randint(6, 14)
-                print('Your constitution is', constitution)
+    def class_constitution(self):
+        if prot == 'warrior': 
+            constitution = random.randint(10, 18)
+            print('Your constitution is', constitution)
+        elif prot == 'priest':
+            constitution = random.randint(10, 18)
+            print('Your constitution is', constitution)
+        elif prot == 'thief': 
+            constitution = random.randint(8, 14)
+            print('Your constitution is', constitution)
+        else:
+            constitution = random.randint(6, 14)
+            print('Your constitution is', constitution)
         
-        def class_dexterity(dexterity):
-            if prot == 'warrior': 
-                dexterity = random.randint(6, 14)
-                print('Your dexterity is', dexterity)
-            elif prot == 'priest':
-                dexterity = random.randint(6, 12)
-                print('Your dexterity is', dexterity)
-            elif prot == 'thief': 
-                dexterity = random.randint(10, 18)
-                print('Your dexterity is', dexterity)
-            else:
-                dexterity= random.randint(4, 12)
-                print('Your dexterity is', dexterity)
+    def class_dexterity(self):
+        if prot == 'warrior': 
+            dexterity = random.randint(6, 14)
+            print('Your dexterity is', dexterity)
+        elif prot == 'priest':
+            dexterity = random.randint(6, 12)
+            print('Your dexterity is', dexterity)
+        elif prot == 'thief': 
+            dexterity = random.randint(10, 18)
+            print('Your dexterity is', dexterity)
+        else:
+            dexterity= random.randint(4, 12)
+            print('Your dexterity is', dexterity)
         
-        def class_intelligence(intelligence):
-            if prot == 'warrior': 
-                intelligence = random.randint(4, 12)
-                print('Your intelligence is', intelligence)
-            elif prot == 'priest':
-                intelligence = random.randint(6, 14)
-                print('Your intelligence is', intelligence)
-            elif prot == 'thief': 
-                intelligence = random.randint(8, 16)
-                print('Your intelligence is', intelligence)
-            else:
-                intelligence = random.randint(12, 18)
-                print('Your intelligence is', intelligence)       
+    def class_intelligence(self):
+        if prot == 'warrior': 
+            intelligence = random.randint(4, 12)
+            print('Your intelligence is', intelligence)
+        elif prot == 'priest':
+            intelligence = random.randint(6, 14)
+            print('Your intelligence is', intelligence)
+        elif prot == 'thief': 
+            intelligence = random.randint(8, 16)
+            print('Your intelligence is', intelligence)
+        else:
+            intelligence = random.randint(12, 18)
+            print('Your intelligence is', intelligence)       
 
-        def starting_hitpoints(hitpoints):
-            self.hitpoints += (self.constitution - 10) * 5
-            print('You have %s hitpoints' % hitpoints)
+    def starting_hitpoints(self):
+        self.hitpoints += (self.constitution - 10) * 5
+        print('You have %s hitpoints' % hitpoints)
 
-        def controller(self):
-            self.charCreate()
-            self.class_strenght()  
+    def controller(self):
+        self.charCreate()
+        self.class_strenght()  
                    
 
